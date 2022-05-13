1eede553a820ef11a3e863e14aab0dfe32dd0024
threadsv1 <majdimed3@gmail.com>
2022-05-13 16:41:49 +0100

    puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Using Puppet, create a file in /tmp
+# Using Puppet, create a file in /tmp.
+
 file { 'school':
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content  => 'I love Puppet',
+  content => 'I love Puppet',
 }
-
