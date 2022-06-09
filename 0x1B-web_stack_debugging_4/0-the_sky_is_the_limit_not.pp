39c03aa0c59dbe07e55251384c7bcd39ebb4d222
threadsv1 <majdimed3@gmail.com>
2022-06-09 19:40:38 +0100

    new file

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# increases the open file limit for nginx
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+  }
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
