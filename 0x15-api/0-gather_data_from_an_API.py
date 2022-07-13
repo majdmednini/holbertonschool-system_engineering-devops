25dea7b137e9538f7e04e33e41e70269b8c6ceb0
threadsv1 <majdimed3@gmail.com>
2022-07-13 23:24:45 +0100

    cmon

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-"""Get todos from user."""
-import requests
-from sys import argv
+"""
+    Python script that, for a given employee ID, returns
+    information about his/her TODO list progress.
+"""
 
-          format(user.get('name'), len(completed_tasks), len(todo)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
+import requests
+import sys
+
 if __name__ == "__main__":
-    todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                         .format(argv[1]))
-    todos = todos.json()
-    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(argv[1]))
-    user = user.json()
-    finished = []
-    for todo in todos:
-        if todo.get('completed'):
-            finished.append(todo)
-    print("Employee {} is done with tasks({}/{}):"
-          .format(user.get('name'), len(finished), len(todos)))
-    for todo in finished:
-        print("\t {}".format(todo.get('title')))
+    id = sys.argv[1]
+    usr_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    tds_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+
+    user = requests.get(usr_url).json()
+    todo = requests.get(tds_url).json()
+
+    completed_nb = 0
+    total_nb = 0
+    completed_tasks = []
+
+    for task in todo:
+        total_nb += 1
+        if task.get("completed") is True:
+            completed_nb += 1
+            completed_tasks.append(task.get("title"))
+
+    sentence = "Employee {} is done with tasks({}/{}):"
+    print(sentence.format(user.get("name"), completed_nb, total_nb))
+    for task in completed_tasks:
+        print("\t {}".format(task))
