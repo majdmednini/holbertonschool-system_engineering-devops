9779a9bbcceb0ee245b8ec91dafd040692c017e7
threadsv1 <majdimed3@gmail.com>
2022-07-13 22:57:57 +0100

    recently added

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fixing Apache is returning a 500 error
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
